f1a92d39e62f8ba5acfa6c0da4d6909877384f87
Ryan kenidy <kenidyryan@gmail.com>
2023-05-11 19:48:07 +0700

    expense context

--- src/contexts/expenseContext.js
+++ src/contexts/expenseContext.js
@@ -2,6 +2,11 @@ import { createContext, useContext, useState } from "react";
 
 const ExpenseContext = createContext();
 const ExpenseUpdateContext = createContext();
+const ExpenseAddContext = createContext();
+
+const ExpensesContext = createContext();
+const ExpensesUpdateContext = createContext();
+const ExpensesAddContext = createContext();
 
 export function useExpense() {
   return useContext(ExpenseContext);
@@ -11,24 +16,51 @@ export function useUpdateExpense() {
   return useContext(ExpenseUpdateContext);
 }
 
+export function useAddExpense() {
+  return useContext(ExpenseAddContext);
+}
+
+export function useExpenses() {
+  return useContext(ExpensesContext);
+}
+
+export function useUpdateExpenses() {
+  return useContext(ExpensesUpdateContext);
+}
+
+export function useAddExpenses() {
+  return useContext(ExpensesAddContext);
+}
+
 export function ExpenseProvider({ children }) {
   const [expense, setExpense] = useState([]);
+  const [expenses, setExpenses] = useState({});
+
+  function updateExpenses(updatedExpenses) {
+    setExpenses(updatedExpenses);
+  }
 
   function addExpense(newExpense) {
-    setExpense((expenses) => [...expenses, { ...newExpense }]);
+    setExpense(expenses => [...expenses, { ...newExpense }]);
   }
 
   function updateExpense(updatedExpense) {
-    setExpense((expenses) => {
-      expenses.find((expense) => (expense.id = updatedExpense.id));
+    setExpense(expenses => {
+      expenses.find(expense => (expense.id = updatedExpense.id));
     });
   }
 
   return (
-    <ExpenseContext.Provider value={expense}>
-      <ExpenseUpdateContext.Provider value={updateExpense}>
-        {children}
-      </ExpenseUpdateContext.Provider>
-    </ExpenseContext.Provider>
+    <ExpensesContext.Provider value={expenses}>
+      <ExpenseContext.Provider value={expense}>
+        <ExpensesUpdateContext.Provider value={updateExpenses}>
+          <ExpenseUpdateContext.Provider value={updateExpense}>
+            <ExpenseAddContext.Provider value={addExpense}>
+              {children}
+            </ExpenseAddContext.Provider>
+          </ExpenseUpdateContext.Provider>
+        </ExpensesUpdateContext.Provider>
+      </ExpenseContext.Provider>
+    </ExpensesContext.Provider>
   );
 }
